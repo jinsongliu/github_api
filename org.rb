ba80fba66fe50db41171c9c24d012bc195f64dca
Jinsong Liu <jinsong.liu@sage.com>
2012-12-03 13:43:09 -0500

    polishing

--- org.rb
+++ org.rb
@@ -12,17 +12,19 @@ def call_api(url)
 end
 
 def get_team_members(id)
-    puts 'Github user id, full name, details'
+    puts @indent + 'Github user id, full name, details'
     members = call_api "/teams/#{id}/members"
     members.each { |m|
         uid = m['login']
         user = call_api "/legacy/user/search/#{uid}"
 
         user = user['users'][0]
-        puts "#{user['username']}, #{user['fullname']}, #{String(user['location']).gsub(',','-')}"
+        puts @indent + "#{user['username']}, #{user['fullname']}, #{String(user['location']).gsub(',','-')}"
     }
 end
 
+@teams = []
+@indent = '   '
 
 def get_teams(org, filter=[])
     teams = call_api "/orgs/#{org}/teams"
@@ -30,7 +32,7 @@ def get_teams(org, filter=[])
         tid = t['id']
         if filter.include?("all") || filter.include?(t['name'])
             puts
-            puts "Team: #{t['name']}"
+            puts "Members in team: #{t['name']}"
             get_team_members tid
         end
     }
@@ -39,15 +41,21 @@ end
 def get_teams_for_repo(org, repo)
     teams = call_api "/repos/#{org}/#{repo}/teams"
 
+    puts
     puts "Teams for repo: #{org}/#{repo}"
     teams = teams.inject([]) { |array, t| array << t['name'] }
-    get_teams org, teams
+    teams.uniq.sort.each {|t|
+        puts @indent + t
+    }
+
+    @teams += teams
+    #get_teams org, teams
 end
 
 def get_repos_for_org(org)
     repos = call_api "/orgs/#{org}/repos"
 
-    puts "Repos for organization: #{org}"
+    #puts "Repos for organization: #{org}"
     repos.each do |repo|
         name = repo['name']
         if block_given?
@@ -62,4 +70,6 @@ def print_user(user)
     puts "#{user['username']}, #{user['fullname']}, #{user['location']}"
 end
 
-get_repos_for_org('org') { |org, repo| get_teams_for_repo org, repo}
+get_repos_for_org('sage') { |org, repo| get_teams_for_repo org, repo}
+
+get_teams 'sage', @teams.uniq.sort
